6628877b11ec95bcfda649c277474c9931c3fb94
Thangavel <U6031021@ten.thomsonreuters.com>
2015-12-15 15:07:07 +0530

    java assignment first day

---
+++ SomeOfTwoNumbers/SomeOfTwoNumbers.java
@@ -0,0 +1,10 @@
+public class SomeOfTwoNumbers
+{
+	public static void main(String[] arg)
+	{
+		int a = 100; //integer variable
+		float b = (int)200.3;//typecasting the float value as integer
+		int sum = a+(int)b; //typecasting the float value as integer
+		System.out.println(sum);
+	}	
+}
---
+++ VariableInitialize/VariableInitialize.java
@@ -0,0 +1,8 @@
+public class VariableInitialize
+{
+	public static void main(String[] arg)
+	{
+		int a,b=4,c;
+		System.out.println(b);
+	}
+}
