2165f3569b7db95f82ccc8e27b266f1519bdcecd
Thangavel <U6031021@ten.thomsonreuters.com>
2015-12-15 15:26:56 +0530

    Java training

---
+++ OperatorsEx/OperatorsEx.java
@@ -0,0 +1,16 @@
+public class OperatorsEx
+{
+	public static void main(String[] arg)
+	{
+		int x = 5, y = 10;
+		int E1,E2,E3,E4;
+		E1 = x + y * 2;
+		E2 = x - y + 2;
+		E3 = (x+y) * 2;
+		E4 = y % x;
+		System.out.println(E1);
+		System.out.println(E2);
+		System.out.println(E3);
+		System.out.println(E4);
+	}
+}
